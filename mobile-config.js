312339a8af0f071930787dc35a75f9000b91b860
Martin Bramwell <martinhbramwell@gmail.com>
2015-02-12 04:17:00 -0500

    use workflow-router as base project

---
+++ mobile-config.js
@@ -0,0 +1,14 @@
+App.info({
+  name: 'smwom',
+  description: 'An Android app built with Meteor',
+  version: '0.0.1'
+});
+
+App.icons({
+  'android_ldpi':    'public/images/icons/doowa_ldpi.png',
+  'android_ndpi':    'public/images/icons/doowa_mdpi.png',
+  'android_hdpi':    'public/images/icons/doowa_hdpi.png',
+  'android_xhdpi':   'public/images/icons/doowa_xhdpi.png',
+  'android_xxhdpi':  'public/images/icons/doowa_xxhdpi.png',
+  'android_xxxhdpi': 'public/images/icons/doowa_xxxhdpi.png'
+});
---
+++ server/app.environment.js
@@ -0,0 +1,8 @@
+
+
+Meteor.methods({
+  getEnvironmentRoot: function(){
+    console.log(process.env.ROOT_URL);
+    return process.env.ROOT_URL;
+  }
+});
